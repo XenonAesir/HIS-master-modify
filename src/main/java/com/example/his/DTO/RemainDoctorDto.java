a7c5aa517803c5faac01601051590508f412fc36
XenonAesir <2393867894@qq.com>
2023-11-05 10:59:37 +0800

    11.05修改：修改完毕

--- src/main/java/com/example/his/DTO/RemainDoctorDto.java
+++ src/main/java/com/example/his/DTO/RemainDoctorDto.java
@@ -1,8 +1,12 @@
 package com.example.his.DTO;
 
+import lombok.Data;
+
 import java.util.Date;
 
-public class RemainDoctorDto {
+@Data
+public class RemainDoctorDto
+{
     private Integer remainDoctorNumberId;
 
     private String departmentId;
@@ -15,51 +19,5 @@ public class RemainDoctorDto {
 
     private String doctorName;
 
-    public Integer getRemainDoctorNumberId() {
-        return remainDoctorNumberId;
-    }
 
-    public void setRemainDoctorNumberId(Integer remainDoctorNumberId) {
-        this.remainDoctorNumberId = remainDoctorNumberId;
-    }
-
-    public String getDoctorId() {
-        return doctorId;
-    }
-
-    public void setDoctorId(String doctorId) {
-        this.doctorId = doctorId;
-    }
-
-    public Date getTime() {
-        return time;
-    }
-
-    public void setTime(Date time) {
-        this.time = time;
-    }
-
-    public Integer getNumber() {
-        return number;
-    }
-
-    public void setNumber(Integer number) {
-        this.number = number;
-    }
-
-    public String getDoctorName() {
-        return doctorName;
-    }
-
-    public void setDoctorName(String doctorName) {
-        this.doctorName = doctorName;
-    }
-
-    public String getDepartmentId() {
-        return departmentId;
-    }
-
-    public void setDepartmentId(String departmentId) {
-        this.departmentId = departmentId;
-    }
 }
